d31a5d676f017e17690add439c86fbf780f2a161
Nikolo <voropn@gmail.com>
2019-09-10 14:14:28 +0300

    2

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ import { happyTickets } from './src/task5byWorker/task5.js';
 import { numericalSequence } from './src/task6.js';
 import { fibonacci, ContextFib } from './src/task7.js';
 import { } from './src/test/test.js';
-
-export { chessBoard, Envelope, Packager, Triangle, sortTriangle, 
+const assert = chai.assert
+export { assert, chessBoard, Envelope, Packager, Triangle, sortTriangle, 
     palindrome, HappyTickets, Context, numericalSequence, fibonacci, ContextFib };
